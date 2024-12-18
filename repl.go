b269e735ff8b0b7dfee45825c041d2f11a2ed450
wfcornelissen <wfcornelissen@gmail.com>
2024-12-18 16:02:15 +0200

    Finished mapping commands

--- repl.go
+++ repl.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*Config) error
 }
 
 var commands map[string]cliCommand
+var pag = 0
+var cfg Config = Config{}
+
+const locationURL = "https://pokeapi.co/api/v2/location-area/?limit=20"
 
 func init() {
 	commands = map[string]cliCommand{
@@ -32,6 +39,11 @@ func init() {
 			description: "Displays the names of next 20 locations",
 			callback:    commandMap,
 		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the names of the previous 20 locations",
+			callback:    commandMapb,
+		},
 	}
 }
 
@@ -48,7 +60,7 @@ func startRepl() {
 		cleanText := cleanInput(text)
 		commandName := cleanText[0]
 		if command, exists := commands[commandName]; exists {
-			err := command.callback()
+			err := command.callback(&cfg)
 			if err != nil {
 				fmt.Println("Error:", err)
 			}
@@ -65,13 +77,13 @@ func cleanInput(text string) []string {
 	return words
 }
 
-func commandExit() error {
+func commandExit(cfg *Config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func helpSectionWrapper() error {
+func helpSectionWrapper(cfg *Config) error {
 	return helpSection(commands)
 }
 
@@ -85,6 +97,69 @@ func helpSection(list map[string]cliCommand) error {
 	return nil
 }
 
-func commandMap() error {
+func commandMap(cfg *Config) error {
+	var linkToGet string
+	if cfg.Next == "" {
+		linkToGet = locationURL
+	} else {
+		linkToGet = cfg.Next
+	}
+	pag += 20
+	cfg.Previous = cfg.Next
+	cfg.Next = locationURL + "&offset=" + strconv.Itoa(pag)
+
+	//HTTP GET request
+	res, err := http.Get(linkToGet)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	// Create var to capture HTTP response into
+	var Response LocationResults
+
+	// Capture HTTP response
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&Response)
+	if err != nil {
+		return err
+	}
+
+	// Create a map of id:name for cities
+	finalList := make(map[int]string)
+
+	for locationID, locationName := range Response.Results {
+		finalList[locationID] = locationName.Name
+	}
+
+	// Debug print
+	for city := range finalList {
+		fmt.Println(finalList[city])
+	}
+
 	return nil
+}
+
+func commandMapb(cfg *Config) error {
+	pag -= 40
+	if cfg.Next == "https://pokeapi.co/api/v2/location-area/?limit=5" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	commandMap(cfg)
+	return nil
+}
+
+type LocationResults struct {
+	Results []Location
+}
+
+type Location struct {
+	Id   int
+	Name string
+}
+
+type Config struct {
+	Next     string
+	Previous string
 }
